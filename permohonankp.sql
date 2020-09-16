bc3ec37a7edc02c2994afa44a23e44f828eb68e4
iqbalatma <iqbalatma@users.noreply.github.com>
2020-09-16 16:33:20 +0700

    revisi database

--- permohonankp.sql
+++ permohonankp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2020 at 12:55 PM
+-- Generation Time: Sep 16, 2020 at 11:32 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -30,19 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `kelompok_mahasiswa` (
   `id_kelompok` int(11) NOT NULL,
   `ketua_kelompok` varchar(64) NOT NULL,
-  `status_kelompok` varchar(64) NOT NULL,
   `deskripsi_kegiatan` text NOT NULL,
   `bidang_kegiatan` varchar(128) NOT NULL,
-  `id_perusahaan` int(11) NOT NULL
+  `status_kelompok` varchar(64) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
+  `id_surat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `kelompok_mahasiswa`
---
-
-INSERT INTO `kelompok_mahasiswa` (`id_kelompok`, `ketua_kelompok`, `status_kelompok`, `deskripsi_kegiatan`, `bidang_kegiatan`, `id_perusahaan`) VALUES
-(31, '10117124', 'Belum Di Approve', 'Membaut absen perusahaan', 'Programmer', 2),
-(32, '10117127', 'Belum Di Approve', 'Membaut absen perusahaan', 'Programmer', 3);
 
 -- --------------------------------------------------------
 
@@ -57,18 +50,6 @@ CREATE TABLE `mahasiswa` (
   `id_kelompok` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `mahasiswa`
---
-
-INSERT INTO `mahasiswa` (`nim_mahasiswa`, `nama_mahasiswa`, `posisi`, `id_kelompok`) VALUES
-('10117124', 'DHIKA NOVIANSYAH', 'Ketua', 31),
-('10117125', 'RIEFKY RAMADHAN', 'Anggota', 31),
-('10117126', 'BAMBANG SIKAMPANG', 'Anggota', 31),
-('10117127', 'JOKO SUTOYO', 'Ketua', 32),
-('10117128', 'JOKO SUTOYO', 'Anggota', 32),
-('10117129', 'JOKO SUSILO SUTOYO', 'Anggota', 32);
-
 -- --------------------------------------------------------
 
 --
@@ -80,18 +61,55 @@ CREATE TABLE `perusahaan` (
   `nama_perusahaan` varchar(128) NOT NULL,
   `alamat_perusahaan` varchar(128) NOT NULL,
   `tanggal_pengajuan` date NOT NULL,
-  `status` varchar(64) NOT NULL
+  `tanggal_disetujui` date NOT NULL,
+  `status_perusahaan` varchar(64) NOT NULL,
+  `jumlah_mahasiswa` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `perusahaan`
 --
 
-INSERT INTO `perusahaan` (`id_perusahaan`, `nama_perusahaan`, `alamat_perusahaan`, `tanggal_pengajuan`, `status`) VALUES
-(1, 'PT Djarum Super', 'Jl. Mahkota Utama', '2020-09-10', 'Belum Di Approve'),
-(2, 'PT Telkom Indonesia', 'Jl Lembong', '2020-09-10', 'Belum Di Approve'),
-(3, 'PT. UNIKOM ', 'DU ', '2020-09-11', 'Belum Di Approve'),
-(4, 'PT. MENCARI CINTA SEJATI', 'Rumah Bill Gates', '2020-09-11', 'Belum Di Approve');
+INSERT INTO `perusahaan` (`id_perusahaan`, `nama_perusahaan`, `alamat_perusahaan`, `tanggal_pengajuan`, `tanggal_disetujui`, `status_perusahaan`, `jumlah_mahasiswa`) VALUES
+(1, 'PT Djarum Super', 'Jl. Mahkota Utama', '2020-09-10', '2020-09-16', 'Sudah Di Approve', 0),
+(2, 'PT Telkom Indonesia', 'Jl Lembong', '2020-09-10', '2020-09-16', 'Sudah Di Approve', 0),
+(3, 'PT. UNIKOM ', 'DU ', '2020-09-11', '2020-09-16', 'Sudah Di Approve', 0),
+(4, 'PT. MENCARI CINTA SEJATI', 'Rumah Bill Gates', '2020-09-11', '2020-09-16', 'Sudah Di Approve', 0),
+(5, 'PT Code Politan', 'Setia Budi', '2020-09-15', '0000-00-00', 'Belum Di Approve', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `surat`
+--
+
+CREATE TABLE `surat` (
+  `id_surat` int(11) NOT NULL,
+  `nomor_surat` int(11) NOT NULL,
+  `status_surat` varchar(64) NOT NULL,
+  `tanggal_pengajuan` date NOT NULL,
+  `tanggal_disetujui` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_admin`
+--
+
+CREATE TABLE `user_admin` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `level` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_admin`
+--
+
+INSERT INTO `user_admin` (`id_user`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'admin', 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -102,7 +120,8 @@ INSERT INTO `perusahaan` (`id_perusahaan`, `nama_perusahaan`, `alamat_perusahaan
 --
 ALTER TABLE `kelompok_mahasiswa`
   ADD PRIMARY KEY (`id_kelompok`),
-  ADD KEY `id_perusahaan` (`id_perusahaan`);
+  ADD KEY `id_perusahaan` (`id_perusahaan`),
+  ADD KEY `id_surat` (`id_surat`);
 
 --
 -- Indexes for table `mahasiswa`
@@ -118,6 +137,19 @@ ALTER TABLE `perusahaan`
   ADD PRIMARY KEY (`id_perusahaan`);
 
 --
+-- Indexes for table `surat`
+--
+ALTER TABLE `surat`
+  ADD PRIMARY KEY (`id_surat`),
+  ADD KEY `Nomor_Surat` (`nomor_surat`);
+
+--
+-- Indexes for table `user_admin`
+--
+ALTER TABLE `user_admin`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -125,13 +157,25 @@ ALTER TABLE `perusahaan`
 -- AUTO_INCREMENT for table `kelompok_mahasiswa`
 --
 ALTER TABLE `kelompok_mahasiswa`
-  MODIFY `id_kelompok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_kelompok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `perusahaan`
 --
 ALTER TABLE `perusahaan`
-  MODIFY `id_perusahaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_perusahaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `surat`
+--
+ALTER TABLE `surat`
+  MODIFY `nomor_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user_admin`
+--
+ALTER TABLE `user_admin`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -141,7 +185,8 @@ ALTER TABLE `perusahaan`
 -- Constraints for table `kelompok_mahasiswa`
 --
 ALTER TABLE `kelompok_mahasiswa`
-  ADD CONSTRAINT `kelompok_mahasiswa_ibfk_1` FOREIGN KEY (`id_perusahaan`) REFERENCES `perusahaan` (`id_perusahaan`);
+  ADD CONSTRAINT `kelompok_mahasiswa_ibfk_1` FOREIGN KEY (`id_perusahaan`) REFERENCES `perusahaan` (`id_perusahaan`),
+  ADD CONSTRAINT `kelompok_mahasiswa_ibfk_2` FOREIGN KEY (`id_surat`) REFERENCES `surat` (`id_surat`);
 
 --
 -- Constraints for table `mahasiswa`
